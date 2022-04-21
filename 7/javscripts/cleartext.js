4432a177a977ab8c87029442d6ebf9cfaa12b619
Ehsan Javadi <54818769+ehsanjavadip@users.noreply.github.com>
2022-04-21 12:47:15 +0430

    Add 7th Project of this Week

---
+++ 7/javscripts/cleartext.js
@@ -0,0 +1,7 @@
+var trash = document.getElementById('trash');
+var txtusername = document.getElementById('txtText');
+
+function ClearText() {
+    txtusername.value='';
+    txtusername.focus();
+}
---
+++ 7/javscripts/helpertext.js
@@ -0,0 +1,11 @@
+var helpertext = document.getElementById('helptext');
+
+function ShowHelperText() {
+    
+    helpertext.style.display = "block";
+}
+
+function HideHelperText() {
+    
+    helpertext.style.display = "none";
+}
